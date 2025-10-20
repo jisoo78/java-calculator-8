0b12c8481bebb9c229e7f510c4be0993a050d9d3
jisoo <ekelldks@naver.com>
2025-10-20 19:01:41 +0900

    feat: 커스텀 구분자 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,12 +5,27 @@ import java.util.Arrays;
 public class Calculator {
     public int add(String numberString) {
 
-        String separator = ",|:";
+        String separator = ",|;";
+
+        if (numberString.startsWith("//")) {
+            int newlineIndex = numberString.indexOf("\n");
+
+            if (newlineIndex == -1) {
+                newlineIndex = numberString.indexOf("\\n");
+            }
+
+            if (newlineIndex != -1) {
+                separator = numberString.substring(2, newlineIndex);
+
+                if (numberString.charAt(newlineIndex) == '\\') {
+                    numberString = numberString.substring(newlineIndex + 2);
+                }
+            }
+        }
+
         String[] numbers = numberString.split(separator);
 
         int sum = Arrays.stream(numbers)
-                .map(String::trim)
-                .filter(s -> !s.isEmpty())
                 .mapToInt(Integer::parseInt)
                 .sum();
 
