bd70aeba7ca7a216d3915753986db71072be0a8b
jisoo <ekelldks@naver.com>
2025-10-20 19:32:48 +0900

    feat: 예외 처리 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,9 +1,10 @@
 package calculator;
 
-import java.util.Arrays;
-
 public class Calculator {
     public int add(String numberString) {
+        if (numberString == null || numberString.isBlank()) {
+            throw new IllegalArgumentException("빈 값을 입력했습니다.");
+        }
 
         String separator = ",|;";
 
@@ -12,6 +13,10 @@ public class Calculator {
 
             if (newlineIndex == -1) {
                 newlineIndex = numberString.indexOf("\\n");
+            }
+
+            if (newlineIndex == -1) {
+                throw new IllegalArgumentException("잘못된 커스텀 구분자를 입력했습니다. 커스텀 구분자는 //과 \\n 사이에 입력해주세요.");
             }
 
             if (newlineIndex != -1) {
@@ -23,12 +28,23 @@ public class Calculator {
             }
         }
 
-        String[] numbers = numberString.split(separator);
+        try {
+            String[] numbers = numberString.split(separator);
+            int sum = 0;
 
-        int sum = Arrays.stream(numbers)
-                .mapToInt(Integer::parseInt)
-                .sum();
+            for (String numberStr : numbers) {
+                numberStr = numberStr.trim();
+                int num = Integer.parseInt(numberStr);
 
-        return sum;
+                if (num < 0) {
+                    throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+                }
+
+                sum += num;
+            }
+            return sum;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("허용되지 않는 값입니다.");
+        }
     }
 }
