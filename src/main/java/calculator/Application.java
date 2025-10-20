db8469978d0161e518eed1521e7d8d367ea72a78
jisoo <ekelldks@naver.com>
2025-10-20 21:30:44 +0900

    fix: 라이브러리 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,14 +1,13 @@
 package calculator;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        String input = Console.readLine();
 
         Calculator calculator = new Calculator();
         int result = calculator.add(input);
